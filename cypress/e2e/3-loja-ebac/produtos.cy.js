d36c0e0849e11f3b767e4304374c37c5e85bb731
Jéssica Silveira <jessica.sdc23@gmail.com>
2024-07-31 16:06:53 -0300

    usando massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,50 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarURL()
     
 });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Apollo Running Short')
-        .click()
-
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
         cy.get('#tab-title-description > a').should('contain','Descrição')
     });
+
+it('Deve buscar um produto com sucesso', () => {
+    let produto = 'Zeppelin Yoga Pant'
+    produtosPage.buscarProduto(produto)
+    cy.get('.product_title').should('contain', produto )
+});
+
+it('Deve visitar a página do produto', () => {
+    produtosPage.visitarProduto('Zeppelin Yoga Pant')
+    cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant' )
+
+});
+
+it('Deve adicionar produto ao carrinho', () => {
+    let qtd = 7
+    produtosPage.buscarProduto('Aether Gym Pant')
+    produtosPage.addProdutoCarrinho('36', 'Blue', qtd)
+    cy.get('.woocommerce-message').should('contain', qtd + ' × “Aether Gym Pant” foram adicionados no seu carrinho.')
+});
+
+it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+    cy.fixture('produtos').then(dados =>{
+    produtosPage.buscarProduto(dados[1].nomeProduto)
+    produtosPage.addProdutoCarrinho(
+        dados[1].tamanho, 
+        dados[1].cor, 
+        dados[1].quantidade)
+    cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+    })
+    
+    
+});
+
+
 });
